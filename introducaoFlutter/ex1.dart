4a5218a97dc40463069fe8b66fff9e4dc5ac077f
Luca Vianna Martins Silveira <luca.vms2004@gmail.com>
2025-03-25 18:33:58 -0300

    Criando pasta para exs de flutter

---
+++ introducaoFlutter/ex1.dart
@@ -0,0 +1,43 @@
+// PARA QUE APENAS O ARQUIVO "MAIN.DART" FOSSE ENVIADO, PRECISEI 
+// COMENTÁ-LO PARA EVITAR WARNINGS DE ERRO!
+
+/*
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Flutter é Legal!",
+            style: TextStyle(
+              color: Colors.white
+            ),
+            ),
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              "Oi mãe >_<"
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+*/
