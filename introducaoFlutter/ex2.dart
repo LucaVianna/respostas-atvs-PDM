a4fdbc990e42baea0c484b1924d313e50685cacf
Luca Vianna Martins Silveira <luca.vms2004@gmail.com>
2025-03-25 19:54:25 -0300

    Alterando o segundo ex...

---
+++ introducaoFlutter/ex2.dart
@@ -0,0 +1,62 @@
+// PARA QUE APENAS O ARQUIVO "MAIN.DART" FOSSE ENVIADO, PRECISEI 
+// COMENTÁ-LO PARA EVITAR WARNINGS DE ERRO!
+
+/*
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Home Page"),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text(
+                "Hello World!",
+                style: TextStyle(
+                  color: Colors.blue, 
+                  fontSize: 24
+                ),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              SizedBox(
+                width: 150,
+                child: ElevatedButton(
+                  onPressed: () {
+                    //Ação do botão
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: Text(
+                    "Um botão",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20
+                    ),
+                  )
+                )
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+*/
