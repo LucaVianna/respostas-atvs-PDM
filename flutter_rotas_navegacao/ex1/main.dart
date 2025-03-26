1042fc56e3c19759f3a2838f6c3382467f2ac4d5
Luca Vianna Martins Silveira <luca.vms2004@gmail.com>
2025-03-26 15:31:38 -0300

    Postando ex de rotas Flutter 2.0

--- flutter_rotas_navegacao/ex1/main.dart
+++ flutter_rotas_navegacao/ex1/main.dart
@@ -1,18 +1,18 @@
-import 'package:flutter/material.dart';
-import 'pages/login_page.dart';
+// import 'package:flutter/material.dart';
+// import 'pages/login_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Login',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginPage(),
-    );
-  }
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Login',
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: LoginPage(),
+//     );
+//   }
   
-}
+// }
--- flutter_rotas_navegacao/ex1/pages/home_page.dart
+++ flutter_rotas_navegacao/ex1/pages/home_page.dart
@@ -1,20 +1,20 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Página inicial'),
-      ),
-      body: Center(
-        child: Text(
-          'Bem-vindo à minha página',
-          style: TextStyle(
-            fontSize: 18,
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Página inicial'),
+//       ),
+//       body: Center(
+//         child: Text(
+//           'Bem-vindo à minha página',
+//           style: TextStyle(
+//             fontSize: 18,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
